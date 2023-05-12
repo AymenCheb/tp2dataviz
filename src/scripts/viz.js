58d3c42b097aa372ece366a8b66effac05f06d31
Maximus Shymko <maximus.shymko@polymtl.ca>
2023-05-11 21:57:19 -0400

    bar chart completed

--- src/scripts/viz.js
+++ src/scripts/viz.js
@@ -8,11 +8,7 @@
  */
 export function updateGroupXScale (scale, data, width) {
   // TODO : Set the domain and range of the groups' x scale
-  const acts = []
-  data.forEach(act => {
-    acts.push(act.Act)
-  })
-  scale.domain(acts).range([0, width])
+  scale.domain(data.map(act => act.Act)).range([0, width])
 }
 
 /**
@@ -24,13 +20,8 @@ export function updateGroupXScale (scale, data, width) {
  */
 export function updateYScale (scale, data, height) {
   // TODO : Set the domain and range of the graph's y scale
-  let maxLineCounts = 0
-  data.forEach(act => {
-    act.Players.forEach(player => {
-      if (player.Count > maxLineCounts) maxLineCounts = player.Count
-    })
-  })
-  scale.domain([maxLineCounts, 0]).range([0, height])
+  const maxLineCount = d3.max(data, act => d3.max(act.Players, p => p.Count))
+  scale.domain([0, maxLineCount]).range([height, 0])
 }
 
 /**
@@ -43,10 +34,9 @@ export function updateYScale (scale, data, height) {
 export function createGroups (data, x) {
   // Select the graph container
   d3.select('#graph-g')
-    .selectAll('.group')
+    .selectAll('g.group')
     .data(data)
-    .enter()
-    .append('g')
+    .join('g')
     .attr('class', 'group')
     .attr('transform', act => `translate(${x(act.Act)}, 0)`)
 }
@@ -65,16 +55,17 @@ export function drawBars (y, xSubgroup, players, height, color, tip) {
   // TODO : Draw the bars
   d3.select('#graph-g')
     .selectAll('.group')
-    .selectAll('.bar')
-    .data(players)
-    .enter()
-    .append('rect')
-    .attr('class', 'bar')
-    .attr('x', player => xSubgroup(player))
-    .attr('y', () => height) // Adjust the y position as needed
-    .attr('width', xSubgroup.bandwidth()) // Adjust the bar width based on the x scale
-    .attr('height', 0) // Initial height of 0
-    .style('fill', player => color(player)) // Use color scale as needed
+    .selectAll('rect')
+    .data(act => players.map(name => ({ // Retrieves the data bound to each group to find the line count for each player
+      Player: name, 
+      Count: act.Players.find(p => p.Player === name)?.Count || 0
+    })))
+    .join('rect')
+    .attr('x', player => xSubgroup(player.Player)) // Position the bars horizontally in a group
+    .attr('y', player => y(player.Count)) // Position the bars vertically
+    .attr('width', xSubgroup.bandwidth()) // Set the width of the bars to the width of each band in the xSubgroup scale
+    .attr('height', player => height - y(player.Count)) // Set the height of the bars
+    .attr('fill', player => color(player.Player)) // Set the appropriate color depending on the player
     .on('mouseover', tip.show) // Show tooltip on mouseover
-    .on('mouseout', tip.hide) // Hide tooltip on mouseout
+    .on('mouseout', tip.hide) // Show tooltip on mouseout
 }
